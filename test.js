f1b3124239bc339077ebbf5ae5915597a74b82a7
sampathreddy29 <sampathreddy2729@gmail.com>
2026-02-04 17:23:01 +0000

    Update test.js

--- test.js
+++ test.js
@@ -1,27 +1,9 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Sum of multiples</title>
-</head>
-<body>
-
-<h3>Enter values</h3>
-
-<label>Factors (comma separated):</label><br>
-<input type="text" id="factors" placeholder="2,3"><br><br>
-
-<label>Multiples (comma separated):</label><br>
-<input type="text" id="multiples" placeholder="4,5,6,7,8,9"><br><br>
-
-<button onclick="calculate()">Calculate</button>
-
-<script>
 function summ(f, m) {
   let sum = 0;
 
   for (let i of m) {
     for (let j of f) {
-      if (i % j === 0) {
+      if (!isNaN(i) && !isNaN(j) && j !== 0 && i % j === 0) {
         sum += i;
         break;
       }
@@ -31,17 +13,15 @@ function summ(f, m) {
 }
 
 function calculate() {
-  const factorsInput = document.getElementById("factors").value;
-  const multiplesInput = document.getElementById("multiples").value;
+  let f = document.getElementById("factors").value
+            .split(',')
+            .map(x => Number(x.trim()));
 
-  const f = factorsInput.split(',').map(Number);
-  const m = multiplesInput.split(',').map(Number);
+  let m = document.getElementById("multiples").value
+            .split(',')
+            .map(x => Number(x.trim()));
 
   let result = summ(f, m);
 
   alert("Result is: " + result);
 }
-</script>
-
-</body>
-</html>
